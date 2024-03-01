5ff5a01790f50f913fabbcd3002d8c4062f25210
lexiealbert78 <la1168@york.ac.uk>
2024-03-01 17:40:07 +0000

    man

--- core/src/com/mygdx/hustle/HeslingtonHustle.java
+++ core/src/com/mygdx/hustle/HeslingtonHustle.java
@@ -1,33 +1,46 @@
 package com.mygdx.hustle;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.math.Rectangle;
 
 public class HeslingtonHustle extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	private Rectangle player;
+	private Texture stillMan;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		stillMan = new Texture(Gdx.files.internal("still man.png"));
+
+		player = new Rectangle();
+		player.x = 800 / 2 - 64 / 2;
+		player.y = 200;
+		player.width = 64;
+		player.height = 64;
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0.5f, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(stillMan, player.x, player.y);
 		batch.end();
-		System.out.println("hello");
-		System.out.println("please");
+
+		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) player.y -= 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Input.Keys.UP)) player.y += 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) player.x -= 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) player.x += 200 * Gdx.graphics.getDeltaTime();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		stillMan.dispose();
 	}
 }
